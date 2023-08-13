ef7f74e9cde0c8161aedd3138f3b6c76d7627725
greenhandzdl <greenhandzdl@gmail.com>
2023-08-13 19:38:03 +0800

    Update data_to_wordcloud.py

--- data_to_wordcloud.py
+++ data_to_wordcloud.py
@@ -1,21 +1,28 @@
 import jieba
-import stylecloud
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
-def ciyun():
-    with open('sources.txt','r',encoding='gbk') as f:
-        word_list = jieba.cut(f.read())
-        result = " ".join(word_list) #分词用空格隔开
-        
-    stylecloud.gen_stylecloud(
-        text=result, # 上面分词的结果作为文本传给text参数
-        size=512,
-        font_path='msyh.ttc', # 字体设置
-        palette='cartocolors.qualitative.Pastel_7', # 调色方案选取，从palettable里选择
-        gradient='horizontal', # 渐变色方向选了垂直方向
-        icon_name='fab fa-weixin',  # 蒙版选取，从Font Awesome里选
-        output_name='test_ciyun.png') # 输出词云图
+def trans_ch(txt):
+  words = jieba.lcut(txt)
+  newtxt = ''.join(words)
+  return newtxt
 
-def main():
-    ciyun()
-if __name__ == '__main__':
-    main()
+
+f = open('data.txt','r',encoding = 'utf-8')     #将你的文本文件名与此句的'maozedong.txt'替换
+txt = f.read()
+f.close
+txt = trans_ch(txt)
+mask = np.array(Image.open("love.png"))               #将你的背景图片名与此句的"love.png"替换
+wordcloud = WordCloud(background_color="white",\
+                      width = 800,\
+                      height = 600,\
+                      max_words = 200,\
+                      max_font_size = 80,\
+                      mask = mask,\
+                      contour_width = 4,\
+                      contour_color = 'steelblue',\
+                        font_path =  "msyh.ttf"
+                      ).generate(txt)
+wordcloud.to_file('result.png')
